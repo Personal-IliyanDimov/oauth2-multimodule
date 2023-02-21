ddff859808c0ef40125e9da20066b47b0beefad3
Personal-IliyanDimov <ilianmd@abv.bg>
2023-02-21 12:45:47 +0200

    Prepare consumer

--- resource-server/src/main/java/org/imd/oauth2/resourceserver/controller/MessagesController.java
+++ resource-server/src/main/java/org/imd/oauth2/resourceserver/controller/MessagesController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MessagesController {
 
+    @PreAuthorize("hasAuthority('message.read')")
     @GetMapping("/messages")
     public String[] getMessages() {
         return new String[] {"Message 1", "Message 2", "Message 3"};
