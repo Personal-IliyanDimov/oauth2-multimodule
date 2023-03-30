8a480a06003bae4122c4b377f7357319d8ec1f7f
Personal-IliyanDimov <ilianmd@abv.bg>
2023-03-30 19:35:14 +0300

    Fix spring boot start issues - remove unneeded settings.gradle files

--- resource-server/src/main/java/org/imd/oauth2/resourceserver/config/CachingConfig.java
+++ resource-server/src/main/java/org/imd/oauth2/resourceserver/config/CachingConfig.java
@@ -7,7 +7,6 @@ import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.swing.*;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
